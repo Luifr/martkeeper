aaab36fc9ca04a66054a18436dff6bdb5c1e4e62
Lui Rocha <ibecobr@gmail.com>
2026-03-29 23:14:32 +0000

    feat: draw person parts

--- scenes/entities/person/Person.cs
+++ scenes/entities/person/Person.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace Martkeeper;
@@ -12,10 +13,36 @@ public partial class Person : RigidBody2D
 	public Hand leftHand;
 	public Hand rightHand;
 
+	private CollisionShape2D HeadShape;
+	private CollisionShape2D LeftShoulderShape;
+	private CollisionShape2D RightShoulderShape;
+
+	[Export] public float headRadius = 40;
+	[Export] public float shoulderRadiusRatio = 0.35f;
+
 	public override void _Ready()
 	{
 		leftHand = GetNode<Hand>("%LeftHand");
 		rightHand = GetNode<Hand>("%RightHand");
+
+		HeadShape = GetNode<CollisionShape2D>("HeadShape");
+		LeftShoulderShape = GetNode<CollisionShape2D>("LeftShoulderShape");
+		RightShoulderShape = GetNode<CollisionShape2D>("RightShoulderShape");
+
+		if (HeadShape.Shape is CircleShape2D circleHeadShape)
+		{
+			circleHeadShape.Radius = headRadius;
+		}
+
+		if (LeftShoulderShape.Shape is CircleShape2D circleLeftShoulderShape)
+		{
+			circleLeftShoulderShape.Radius = headRadius * shoulderRadiusRatio;
+		}
+
+		if (RightShoulderShape.Shape is CircleShape2D circleRightShoulderShape)
+		{
+			circleRightShoulderShape.Radius = headRadius * shoulderRadiusRatio;
+		}
 	}
 
 	public override void _Process(double delta)
@@ -27,5 +54,21 @@ public partial class Person : RigidBody2D
 		Vector2 targetVelocity = MoveInput * Speed;
 
 		LinearVelocity = LinearVelocity.Lerp(targetVelocity, 0.1f);
+	}
+
+	public override void _Draw()
+	{
+		var headColor = Color.Color8(0, 0, 0);
+		var shirtColor = Color.Color8(0, 255, 0);
+
+		// Shoulder
+		DrawCircle(new Vector2(0, 35), headRadius * shoulderRadiusRatio, shirtColor);
+		DrawCircle(new Vector2(0, -35), headRadius * shoulderRadiusRatio, shirtColor);
+
+		// Nose
+		DrawCircle(new Vector2(headRadius, 0), headRadius * shoulderRadiusRatio * shoulderRadiusRatio, headColor);
+
+		// Head
+		DrawCircle(Vector2.Zero, headRadius, headColor);
 	}
 }
