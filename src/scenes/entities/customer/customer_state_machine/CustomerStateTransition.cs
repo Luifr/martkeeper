4493d3928a3bd0de52253f850f71bd99a1e69e60
Lui Rocha <ibecobr@gmail.com>
2026-04-05 22:44:23 +0000

    fix: from to order in transition

--- src/scenes/entities/customer/customer_state_machine/CustomerStateTransition.cs
+++ src/scenes/entities/customer/customer_state_machine/CustomerStateTransition.cs
@@ -1,7 +1,7 @@
 namespace Martkeeper.Entities;
 
-public abstract class CustomerStateTransition(CustomerStateName to, CustomerStateName from)
+public abstract class CustomerStateTransition(CustomerStateName from, CustomerStateName to)
 {
-  public readonly CustomerStateName To = to;
   public readonly CustomerStateName From = from;
+  public readonly CustomerStateName To = to;
 }
